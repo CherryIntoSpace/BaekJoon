bf386e7d53bd6fdb1b29cff62c9d5e9bbadb8c93
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-03-19 19:12:51 +0900

    Update 이상한 기호.cc

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -2,7 +2,7 @@
 #include <algorithm>
 using namespace std; 
 int main(int argc, char** argv) {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int a,b;
+	long long a,b;
 	cin>>a>>b;
 	
 	cout<<(a+b)*(a-b);
