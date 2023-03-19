089568c67ca33bd76cb87ff58824e42638a0eca5
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-03-19 19:18:28 +0900

    [Bronze V] Title: 엔드게임 스포일러, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17295. 엔드게임 스포일러/엔드게임 스포일러.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <algorithm>
+using namespace std; 
+int main(int argc, char** argv) {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	string s;
+	cin>>s;
+	
+	cout<<"Avengers: Endgame";
+}
