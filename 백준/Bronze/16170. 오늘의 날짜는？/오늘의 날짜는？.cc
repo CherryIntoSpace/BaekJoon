311c6d8bd62ded080e9e6fd93540838234d8b838
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-03-21 20:52:39 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main(int argc, char** argv) {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cout<<2023<<"\n"<<"03"<<"\n"<<21;
+}
