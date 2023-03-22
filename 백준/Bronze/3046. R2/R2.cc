f110a35f7c21d59aa48d5e20e396a4bdf290eb17
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-03-23 01:43:22 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	int R1, S;
+	
+	cin>>R1>>S;
+	
+	cout<<(S * 2) - R1;
+}
