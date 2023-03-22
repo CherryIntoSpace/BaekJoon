2f05a05a3ac3bc1ee48cfec4f2e86aff4c161332
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-03-22 22:32:09 +0900

    [Bronze V] Title: The World Responds, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char** argv) {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cout<<"The world says hello!";
+}	
