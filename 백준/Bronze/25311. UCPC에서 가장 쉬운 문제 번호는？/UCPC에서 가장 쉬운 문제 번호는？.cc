5ff8f120e5c424e9447e4c790e62afc03a64d79e
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-03-22 22:51:41 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char** argv) {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int y;
+	cin>>y;
+	
+	cout<<"A";
+}
