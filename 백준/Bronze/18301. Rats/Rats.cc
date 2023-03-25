e10bdf8285780945357e4f3f8bc6e5ae20488e7b
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-03-25 19:34:43 +0900

    [Bronze V] Title: Rats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n1, n2, n3;
+	
+	cin>>n1>>n2>>n3;
+	
+	cout<<(n1 + 1) * (n2 + 1) / (n3 + 1) - 1;
+}
