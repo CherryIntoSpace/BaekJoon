3a97b12decee8e1ef2d8064164900cf7dbfda4b9
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-03-26 04:57:26 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	int x;
+	
+	cin>>x;
+	
+	cout<<x * 24;
+}
