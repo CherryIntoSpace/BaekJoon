40ec732cb5904b822972ed2eca203a35616759e3
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-04-02 01:30:26 +0900

    [Bronze IV] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char** argv) {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	char c;
+	
+	cin>>c;
+	
+	if((c == 'N') || (c == 'n')){
+		cout<<"Naver D2";
+	}
+	else{
+		cout<<"Naver Whale";
+	}
+}
