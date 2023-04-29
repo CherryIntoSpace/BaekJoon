15fa2acb05f7123afbeca917d54bbbdd9aa0fd31
CherryIntoSpace <127698945+CherryIntoSpace@users.noreply.github.com>
2023-04-29 22:21:44 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cmath>
+using namespace std;
+int main(int argc, char** argv) {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	int n,m;
+	
+	cin>>n>>m;
+	
+	(n * 100 >= m) ? cout<<"Yes" :  cout<<"No";
+}
